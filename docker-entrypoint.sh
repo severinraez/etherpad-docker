7343fada3ff978923546a501d0a82e7ce8701fc6
Jose Castro Leon <jose.castro.leon@cern.ch>
2017-11-28 11:03:13 +0100

    Use symlinks instead of copying the secret

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,17 @@
 # If the settings file variable has been set
 # overwrite the contents of the settings
 if [ ! -z ${SETTINGS_FILE:-} ]; then
-    \cp -rf ${SETTINGS_FILE} settings.json
+    rm -rf settings.json && ln -s ${SETTINGS_FILE} settings.json
 fi
 
 # Same procedure with the session key
 if [ ! -z ${SESSIONKEY_FILE:-} ]; then
-    \cp -rf ${SESSIONKEY_FILE} SESSIONKEY.txt
+    rm -rf SESSIONKEY.txt && ln -s ${SESSIONKEY_FILE} SESSIONKEY.txt
 fi
 
 # Same procedure with the API key
 if [ ! -z ${APIKEY_FILE:-} ]; then
-    \cp -rf ${APIKEY_FILE} APIKEY.txt
+    rm -rf APIKEY.txt && ln -s ${APIKEY_FILE} APIKEY.txt
 fi
 
 # Execute the etherpad provided startup script
