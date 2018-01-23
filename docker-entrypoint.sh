08c08b5f1c7bf69cc75e79aa9ddc8e9ca58a60c3
srz <severin.raez@bluewin.ch>
2018-01-23 18:48:23 +0100

    Take config from env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+
 # If the settings file variable has been set
 # overwrite the contents of the settings
 if [ ! -z ${SETTINGS_FILE:-} ]; then
     rm -rf settings.json && ln -s ${SETTINGS_FILE} settings.json
 fi
+
+TITLE=${TITLE:-"Etherpad"}
+
+if [ -z "$DB_USER" ] || [ -z "$DB_HOST" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_NAME" ] || [ -z "$DB_TYPE" ]; then
+    echo "One of the environment variables DB_USER DB_HOST DB_PASSWORD DB_NAME DB_TYPE is not set."
+    exit 1
+fi
+
+sed -i s/DB_USER/$DB_USER/g settings.json
+sed -i s/DB_HOST/$DB_HOST/g settings.json
+sed -i s/DB_PASSWORD/$DB_PASSWORD/g settings.json
+sed -i s/DB_NAME/$DB_NAME/g settings.json
+sed -i s/DB_TYPE/$DB_TYPE/g settings.json
+sed -i s/TITLE/$TITLE/g settings.json
 
 # Same procedure with the session key
 if [ ! -z ${SESSIONKEY_FILE:-} ]; then
